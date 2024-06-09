d1354446b7bb4ad215e112e7e2470140b1e7d024
Shalahuddin Ahmad Aziz <shalahuddinahmad.aziz@gmail.com>
2024-06-09 14:55:54 +0700

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Shalahuddin Ahmad Aziz on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
